dc6c928c10dd04319531f5804b0c9db8c184eba0
lukasjaronis <jaronisluke13@gmail.com>
2019-10-14 15:08:21 -0400

    completed objects.js, challenge 2 done on arrays

--- assignments/objects.js
+++ assignments/objects.js
@@ -10,6 +10,7 @@
 // 5, adaine5@samsung.com, Antonietta, F
 
 // Example format of an intern object: 1, examples@you.edu, Example, F
+/*  
 const example = {
   id: 0,
   name: "Example",
@@ -17,8 +18,53 @@ const example = {
   gender: "F",
 }
 
+*/
+
 // Write your intern objects here:
 
+
+const internMitzi = {
+  id: 0,
+  email: 'mmelloy0@psu.edu',
+  name: 'Mitzi',
+  gender: 'F',
+};
+
+const internKennan = {
+  id: 1,
+  email: 'kdiben1@tinypic.com',
+  name: 'Kennan',
+  gender: 'M',
+  speak: function() {
+    return 'I can speak!';
+  },
+};
+
+const internKeven = {
+  id: 2,
+  email: 'kmummery2@wikimedia.org',
+  name: 'Keven',
+  gender: 'M',
+};
+
+const internGannie = {
+  id: 3,
+  email: 'gmartinson3@illinois.edu',
+  name: 'Gannie',
+  gender: 'M',
+};
+
+const internAntonietta = {
+  id: 4,
+  email: 'adaine5@samsung.com',
+  name: 'Antonietta',
+  gender: 'F',
+  mutiply: function(a, b) {
+    return a * b;
+  }
+};
+
+console.log(internMitzi, internKennan, internKeven, internGannie, internAntonietta);
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
@@ -33,12 +79,20 @@ const example = {
 
 // Antonietta's Gender
 
+
+console.log(internMitzi.name, internKennan.id,internKeven.email, internGannie.name, internAntonietta.gender);
+
+
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+console.log(internKennan.speak());
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+console.log(internAntonietta.mutiply(5, 5));
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
